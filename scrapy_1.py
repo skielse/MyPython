7260fa252d775ec007e7f8bb1f73366adf0f7fff
skielse <marine0228@gmail.com>
2015-04-13 20:36:23 +0800

    4.13 test2

--- scrapy_1.py
+++ scrapy_1.py
@@ -1,15 +1,24 @@
 # -*- coding:utf-8 -*-
 __author__ = 'wangzijie'
 import urllib2
+import re
 
 page = 1
 url = 'http://www.qiushibaike.com/hot/page/' + str(page)
 user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
 headers = {'User-Agent': user_agent}
 try:
-	request = urllib2.Request(url, headers = headers)
+	request = urllib2.Request(url, headers=headers)
 	response = urllib2.urlopen(request)
-	print response.read()
+	content = response.read().decode('utf-8')
+	pattern = re.compile('<div.*?class="author.*?>.*?<a.*?</a>.*?<a.*?>(.*?)</a>.*?<div.*?class'
+	                     '="content".*?title="(.*?)">(.*?)</div>(.*?)<div class="stats.*?class="number">(.*?)</i>',
+	                     re.S)
+	items = re.findall(pattern, content)
+	for item in items:
+		haveImg = re.search("img", item[3])
+		if not haveImg:
+			print item[0], item[1], item[2], item[3], item[4]
 except urllib2.URLError, e:
 	if hasattr(e, "code"):
 		print e.code
