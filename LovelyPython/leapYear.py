88ebdf17626f6c5983271a3da15522eb805cc923
skielse <marine0228@gmail.com>
2017-01-11 22:26:12 +0800

    在家测试修改1

--- LovelyPython/leapYear.py
+++ LovelyPython/leapYear.py
@@ -8,7 +8,6 @@ re1 = year % 400
 re2 = (year % 4)
 re3 = (year % 100)
 if re1 == 0 or (re2 == 0 and re3 != 0):
-	print(str(year) + " is leap year !")
+    print(str(year) + " is leap year !")
 else:
-	print(str(year) + " is not leap year !!")
-
+    print(str(year) + " is not leap year !")
